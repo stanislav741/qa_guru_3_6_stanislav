d5c2d221ad6eb8c0d2918675f262698ea7cda03e
stanislavd <stasdmitruk1@gmail.com>
2021-02-05 03:54:58 +0100

    Updated file read & assert tests

--- src/test/java/tests/TxtTests.java
+++ src/test/java/tests/TxtTests.java
@@ -1,22 +1,34 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import utils.FileUtils;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.title;
+import static com.codeborne.selenide.FileDownloadMode.PROXY;
+import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TxtTests {
 
     @Test
     void verifyContentInTxt(){
-        File file = new File("src/test/resources/html/index.html");
-        String filePath = file.getAbsolutePath();
+//        Configuration.proxyEnabled = true;
+//        Configuration.fileDownload = PROXY;
+//        Configuration.reportsFolder = "";
 
-        open(filePath);
-        assertTrue(title().equals("QA.GURU examples"));
+        String expectedFileText = "Test text line";
 
+//        String filePath = file.getAbsolutePath();
+//        open(filePath);
+//        assertTrue(title().equals("QA.GURU examples"));
+//        $("#txt").download();
+
+        File file = new File("src/test/resources/html/1.txt");
+        String actualFileText = new FileUtils().readStringFromFile(file.getPath());
+        System.out.println("Actual text from file: \n" + actualFileText);
+
+        assertTrue(actualFileText.contains(expectedFileText));
     }
 }
